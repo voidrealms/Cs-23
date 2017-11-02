d64bcac965fcdde518d561cdad9ffb1ed21a002a
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:23:24 -0400

    First commit

---
+++ CS 23/CS 23/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Do_Checked();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Do_Checked();
+        }
+
+        private void Do_Checked()
+        {
+            button1.Enabled = checkBox1.Checked;
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: C# 23 - Checkbox
+Published: 9/29/2011 3:38:56 PM
+Link: https://www.youtube.com/watch?v=bcytmYo1FDY
+
+Description: Learn how to use the checkbox
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
